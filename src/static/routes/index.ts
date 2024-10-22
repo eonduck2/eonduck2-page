18278f69194abcf1cdd21af99734e94d7c63ead9
이종수 <whdtnxd@gmail.com>
2024-10-22 18:12:17 +0900

    :package: 페이지 정의 완료

--- src/static/routes/index.ts
+++ src/static/routes/index.ts
@@ -2,7 +2,7 @@ import { TProject } from "~/types/project/project.type";
 
 export const projects: TProject[] = [
   {
-    id: 5,
+    id: 1,
     title: "Eco Vision",
     description: "야생 동물 모니터링 및 출몰 알림 서비스",
     image: "/images/eco_vision.png",
@@ -14,7 +14,7 @@ export const projects: TProject[] = [
     category: "팀",
   },
   {
-    id: 4,
+    id: 2,
     title: "Workids",
     description: "저출산, 고령화에 필요한 첨단 디지털 서비스 개발",
     image: "/images/workids.png",
@@ -39,7 +39,7 @@ export const projects: TProject[] = [
     category: "팀",
   },
   {
-    id: 2,
+    id: 4,
     title: "Dream Vending Machine",
     description: "세상에 없는 자판기 구현",
     image: "/images/dream_vending.png",
@@ -52,7 +52,7 @@ export const projects: TProject[] = [
     category: "팀",
   },
   {
-    id: 1,
+    id: 5,
     title: "Team.gg",
     description: '온라인 게임 "리그 오브 레전드"의 전적 검색 웹 서비스.',
     image: "/images/teamGG.png",
@@ -62,6 +62,100 @@ export const projects: TProject[] = [
       "https://kongukjae.notion.site/Team-gg-2fb51b32fdd644e4b95064b4834c8484?pvs=4",
     githubLink: "https://github.com/eonduck2/Team.gg",
     category: "팀",
+  },
+  {
+    id: 6,
+    title: "YT-UP",
+    description: "유튜브 사용자들에게 상세 데이터들을 제공하는 유틸리티 서비스",
+    image: "/images/yt_up.png",
+    duration: "24.09.09 ~ ing ...",
+    technologies: ["Qwik", "Go", "Redis"],
+    aboutLink:
+      "https://sharing.clickup.com/9014616582/b/h/5-90143610828-2/d7c39f4c306d275",
+    githubLink: "https://github.com/eonduck2/ioyoa-fe",
+    category: "개인",
+  },
+  {
+    id: 7,
+    title: "Tarantula",
+    description:
+      "개인 프로젝트에 사용할 유튜브 관련 정보들을 수집하는 크롤러 구현",
+    image: "/images/tarantula.png",
+    duration: "24.09.11 ~ 24.09.11",
+    technologies: ["python"],
+    aboutLink: "",
+    githubLink: "https://github.com/eonduck2/tarantula",
+    category: "개인",
+  },
+  {
+    id: 8,
+    title: "Chaining-Tracker",
+    description: "Chaining된 value를 추적할 수 있는 VSC 확장 프로그램 개발",
+    image: "/images/chaining_tracker.png",
+    duration: "24.06.07 ~ 24.06.11",
+    technologies: ["TypeScript"],
+    aboutLink:
+      "https://freckle-nasturtium-6d7.notion.site/Chainig-Tracker-8dd5f5ee1ac641b581b938141ceef2b3?pvs=4",
+    githubLink: "https://github.com/eonduck2/chaining-tracker",
+    category: "개인",
+  },
+  {
+    id: 9,
+    title: "Blog",
+    description: "개인적으로 사용할 수 있는 블로그 개발",
+    image: "/images/blog.png",
+    duration: "24.05.24 ~ 24.06.06",
+    technologies: ["HTML5", "JavaScript"],
+    aboutLink:
+      "https://freckle-nasturtium-6d7.notion.site/1f74593719284f0f913e16c4ecb90a27?pvs=73",
+    githubLink: "https://github.com/eonduck2/Blog",
+    category: "개인",
+  },
+  {
+    id: 10,
+    title: "포켓 북",
+    description: "Poke API를 이용한 포켓몬 도감 구현",
+    image: "/images/pokemon.png",
+    duration: "24.05.12 ~ 24.05.20",
+    technologies: ["HTML5", "Less.js", "JavaScript"],
+    aboutLink:
+      "https://freckle-nasturtium-6d7.notion.site/81da5b16439c41999df391e6da5ee3c8?pvs=73",
+    githubLink: "https://github.com/eonduck2/pokemon",
+    category: "개인",
+  },
+  {
+    id: 11,
+    title: "나루토 캐릭터 카드",
+    description:
+      "narutoDB API를 이용한 가장 좋아하는 나루토 캐릭터 5명의 캐릭터 카드 구현",
+    image: "/images/favorite_naruto.png",
+    duration: "24.05.11 ~ 24.05.11",
+    technologies: ["HTML5", "SCSS", "JavaScript", "Node.js"],
+    aboutLink: "https://eonduck2.github.io/favorite_naruto_character/",
+    githubLink: "https://github.com/eonduck2/favorite_naruto_character",
+    category: "개인",
+  },
+  {
+    id: 12,
+    title: "아이폰 15 클론",
+    description: "아이폰 15 클론 페이지 구현",
+    image: "/images/iphone15.png",
+    duration: "24.04.09 ~ 24.04.16",
+    technologies: ["HTML5", "CSS3"],
+    aboutLink: "https://eonduck2.github.io/iphone15Clone/",
+    githubLink: "https://github.com/eonduck2/iphone15Clone",
+    category: "개인",
+  },
+  {
+    id: 13,
+    title: "랜덤 음악 추천기",
+    description: "최근에 자주 듣는 음악들을 선정해서 랜덤하게 추천",
+    image: "/images/random_music.png",
+    duration: "24.04.03 ~ 24.04.05",
+    technologies: ["HTML3", "CSS3", "JavaScript"],
+    aboutLink: "https://eonduck2.github.io/random_music_recommend/",
+    githubLink: "https://github.com/eonduck2/random_music_recommend",
+    category: "개인",
   },
 ];
 
